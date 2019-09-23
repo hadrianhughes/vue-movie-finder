0c82cc8a7a356c91abb83685ad7c79702c341a3d
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-09-23 10:00:08 +0100

    Create Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyPlugin([
+      { from: 'static' }
+    ])
+  ]
+};
